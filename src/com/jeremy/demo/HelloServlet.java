1a8a7fefddfc2cf1aa41304c09f84fa132ff48c7
jeremy <277068385@qq.com>
2023-02-11 21:54:27 +0800

    Initial commit

---
+++ src/com/jeremy/demo/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.jeremy.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author ningshunbin
+ * @date 2023-02-11 - 21:27
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("hello servlet!");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: nings
+  Date: 2023-02-11
+  Time: 21:15
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>首页</title>
+  </head>
+  <body>
+  这是java web project的首页！
+  </body>
+</html>
